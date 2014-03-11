af993cf4ab4846da7c7ca37a6d4fd461504eab31
TheRoboDC <robodcvideos@gmail.com>
2014-03-10 22:03:51 -0400

    Source Files

--- src/io/github/TheRoboDC/EffectsListener.java
+++ src/io/github/TheRoboDC/EffectsListener.java
@@ -2,11 +2,13 @@ package io.github.TheRoboDC;
 
 import org.bukkit.ChatColor;
 import org.bukkit.World;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.Location;
 
 public class EffectsListener implements Listener {
@@ -25,6 +27,16 @@ public class EffectsListener implements Listener {
 		if(drop.getItemStack().getTypeId() == 46) {
 			player.sendMessage(ChatColor.GREEN + "You threw a grenade!");
 			world.createExplosion(loc, 0.0F);
+		}
+	}
+	
+	@EventHandler	
+	public void onFishCast(PlayerFishEvent event) {
+		Player player = event.getPlayer(); //Gets the player who casted the fishing rod.
+		Entity target = event.getCaught();
+		if (target instanceof Player) {
+			player.teleport(target);
+			player.sendMessage(ChatColor.GREEN + "You have successfully teleported to your target!");
 		}
 		
 	}
